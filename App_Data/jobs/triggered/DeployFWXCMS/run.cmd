70a0d19c9b9b8f1d96cc163c40bcef6cf4802d1e
Fyneworks <dev@fyneworks.com>
2016-02-07 13:25:28 +0000

    updated fwx pull script

--- App_Data/jobs/triggered/DeployFWXCMS/run.cmd
+++ App_Data/jobs/triggered/DeployFWXCMS/run.cmd
@@ -1,4 +1,6 @@
-cd /d %HOME%\fwxcms
-git clone https://github.com/pinesandneedles/fwx-system.git
+cd /home/site
+md fwxcms
+cd fwxcms
+git clone https://github.com/pinesandneedles/fwx-system.git .
 git pull
 echo "hello"
